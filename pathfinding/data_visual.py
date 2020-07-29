64de53d5e51212669c9f9bb6058515a43962bf9e
rotemyoeli <42690752+rotemyoeli@users.noreply.github.com>
2020-07-29 17:15:16 +0300

    :)

--- pathfinding/data_visual.py
+++ pathfinding/data_visual.py
@@ -6,44 +6,34 @@ from tkinter.filedialog import askdirectory
 import glob
 import os
 
+# plt.gca().set_color_cycle(['red', 'green', 'blue', 'yellow'])
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-path = askdirectory() # show an "Open" dialog box and return the path to the selected file
+Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+path = askdirectory()  # show an "Open" dialog box and return the path to the selected file
 all_files = glob.glob(path + "/*.csv")
-
 
 paths = []
 for root, dirs, files in os.walk(path):
     for file in files:
         if file.endswith(".csv") and ('03-mdr_on_normal_paths' in root):
-            print(root, dirs)
-            print(os.path.join(root, file))
             s = os.path.join(root, file)
             print(s)
             paths.append(s)
 
 all_data = []
+
 for filename in paths:
+    file_data = []
     df = pd.read_csv(filename)
+    file_data.append(df)
+    frame = pd.concat(file_data, axis=0, ignore_index=True)
+    df2 = frame.groupby(frame.adv_damage_steps)['mdr_run_time_seconds'].mean()
+    df2.plot()
     all_data.append(df)
 
-
-#li = []
-
-#for filename in all_files:
-#    df = pd.read_csv(filename, index_col=None, header=0)
-#    li.append(df)
-
-frame = pd.concat(all_data, axis=0, ignore_index=True)
-
-
-#df = pd.read_csv(filename)
-
-#df2 = df.groupby(df.paths_file_name.str[:21])['adv_agent_id'].count()
-df2 = frame.groupby(frame.ms_mdr_path)['mdr_run_time_seconds'].mean()
-df2.plot.line()
-
-#df2.plot(kind='line', x='adv_agent_ds',y='mdr_online_const_run_time_seconds',color='green')
-#plt.show()
+plt.legend(['Agents = 3', 'Agents = 2'], loc='upper left')
+plt.ylabel('Time in Sec.')
+plt.xlabel('Number of Damage Steps')
+plt.show()
 
 print(df2)
